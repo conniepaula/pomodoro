a93450e15351d0f8c07d0db46b87b02a497edb0f
Connie <conniedpaula@gmail.com>
2023-05-13 23:19:34 -0300

    Display task cycle information in History route

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,64 +1,9 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from "date-fns";
+import enGB from "date-fns/locale/en-GB";
+
 import { TaskContext } from "../../context/TaskContext";
 import { HistoryContainer, HistoryList, Status } from "./History.styles";
-
-type Task = {
-  taskName: string;
-  duration: string;
-  startTime: string;
-  status: "Complete" | "Pending" | "Cancelled";
-};
-
-const tasks: Array<Task> = [
-  {
-    taskName: "Code review",
-    duration: "30 minutes",
-    startTime: "1 day ago",
-    status: "Complete",
-  },
-  {
-    taskName: "Bug fixing",
-    duration: "60 minutes",
-    startTime: "1 day ago",
-    status: "Pending",
-  },
-  {
-    taskName: "Team meeting",
-    duration: "45 minutes",
-    startTime: "3 hours ago",
-    status: "Complete",
-  },
-  {
-    taskName: "Unit testing",
-    duration: "20 minutes",
-    startTime: "1 day ago",
-    status: "Complete",
-  },
-  {
-    taskName: "Documentation",
-    duration: "45 minutes",
-    startTime: "2 days ago",
-    status: "Cancelled",
-  },
-  {
-    taskName: "Feature development",
-    duration: "120 minutes",
-    startTime: "1 day ago",
-    status: "Pending",
-  },
-  {
-    taskName: "Research",
-    duration: "30 minutes",
-    startTime: "3 days ago",
-    status: "Complete",
-  },
-  {
-    taskName: "Code refactoring",
-    duration: "60 minutes",
-    startTime: "2 days ago",
-    status: "Complete",
-  },
-];
 
 function History() {
   const { taskCycles } = useContext(TaskContext);
@@ -76,14 +21,26 @@ function History() {
             </tr>
           </thead>
           <tbody>
-            {tasks.map((task) => (
-              <tr key={task.taskName}>
-                <td>{task.taskName}</td>
-                <td>{task.duration}</td>
-                <td>{task.startTime}</td>
-                <Status status={task.status}>{task.status}</Status>
-              </tr>
-            ))}
+            {taskCycles.map((task) => {
+              const status = task.endDate
+                ? "Complete"
+                : task.stopDate
+                ? "Cancelled"
+                : "Pending";
+              return (
+                <tr key={task.id}>
+                  <td>{task.task}</td>
+                  <td>{task.duration}</td>
+                  <td>
+                    {formatDistanceToNow(task.startDate, {
+                      addSuffix: true,
+                      locale: enGB,
+                    })}
+                  </td>
+                  {<Status status={status}>{status}</Status>}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </HistoryList>
