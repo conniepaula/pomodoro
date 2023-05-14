53af10dc81c8db5b45569bc012b0b5b7a76d582e
Connie <conniedpaula@gmail.com>
2023-05-13 22:04:06 -0300

    Start adding features to the app: task tracking, working countdown

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,17 @@
-import { PlayIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import { PlayIcon, StopIcon } from "@radix-ui/react-icons";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { differenceInSeconds } from "date-fns";
 
 import {
-  ButtonContainer,
   Colon,
   DurationInput,
   FormContainer,
   HomeContainer,
+  StartButtonContainer,
+  StopButtonContainer,
   TaskInput,
   TimerContainer,
 } from "./Home.styles";
@@ -17,13 +20,25 @@ const newTaskValidationSchema = z.object({
   task: z.string().min(1, "Give your task a name"),
   duration: z
     .number()
-    .min(5, "Duration cannot be shorter than 5 minutes")
+    .min(1, "Duration cannot be shorter than 5 minutes")
     .max(60, "Duration cannot be longer than 60 minutes"),
 });
 
 type TaskSchema = z.infer<typeof newTaskValidationSchema>;
 
+interface TaskCycle {
+  id: string;
+  task: string;
+  duration: number;
+  startDate: Date;
+  endDate?: Date;
+  stopDate?: Date;
+}
+
 function Home() {
+  const [taskCycles, setTaskCycles] = useState<Array<TaskCycle>>([]);
+  const [activeTaskId, setActiveTaskId] = useState<string | null>(null);
+  const [secondsPassed, setSecondsPassed] = useState<number>(0);
   const { register, handleSubmit, watch, reset } = useForm<TaskSchema>({
     resolver: zodResolver(newTaskValidationSchema),
     defaultValues: {
@@ -31,26 +46,102 @@ function Home() {
       duration: 25,
     },
   });
-  const handleCreateNewTask = (data: TaskSchema) => {
-    console.log(data);
-    reset();
-  };
+
+  const activeTask = taskCycles.find(
+    (taskCycle) => taskCycle.id === activeTaskId
+  );
+  const durationInSeconds = activeTask ? activeTask.duration * 60 : 0;
+  const currentSeconds = activeTask ? durationInSeconds - secondsPassed : 0;
+  const minutes = Math.floor(currentSeconds / 60);
+  const seconds = currentSeconds % 60;
+
+  const minutesCountdown = String(minutes).padStart(2, "0");
+  const secondsCountdown = String(seconds).padStart(2, "0");
+
   const task = watch("task");
   const isSubmitDisabled = !task;
 
+  useEffect(() => {
+    let interval: number;
+    if (activeTask) {
+      interval = setInterval(() => {
+        const diffInSeconds = differenceInSeconds(
+          new Date(),
+          activeTask.startDate
+        );
+        if (diffInSeconds >= durationInSeconds) {
+          setTaskCycles((prevTaskCycles) =>
+            prevTaskCycles.map((task) => {
+              if (task.id === activeTaskId) {
+                return { ...task, endDate: new Date() };
+              } else {
+                return task;
+              }
+            })
+          );
+          setSecondsPassed(durationInSeconds);
+          clearInterval(interval);
+        } else {
+          setSecondsPassed(diffInSeconds);
+        }
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeTask, durationInSeconds, activeTaskId]);
+
+  const handleCreateNewTaskCycle = (data: TaskSchema) => {
+    const taskId = String(new Date().getTime());
+
+    const newTaskCycle: TaskCycle = {
+      id: taskId,
+      task: data.task,
+      duration: data.duration,
+      startDate: new Date(),
+    };
+
+    setTaskCycles((prevTaskCycles) => [...prevTaskCycles, newTaskCycle]);
+    setActiveTaskId(taskId);
+    setSecondsPassed(0);
+
+    reset();
+  };
+
+  const handleStopTaskCycle = () => {
+    setTaskCycles((prevTaskCycles) =>
+      prevTaskCycles.map((task) => {
+        if (task.id === activeTaskId) {
+          return { ...task, stopDate: new Date() };
+        } else {
+          return task;
+        }
+      })
+    );
+
+    setActiveTaskId(null);
+  };
+
+  useEffect(() => {
+    if (activeTask) {
+      document.title = `${activeTask.task} - ${minutesCountdown}:${secondsCountdown}`;
+    }
+  }, [activeTask, minutesCountdown, secondsCountdown]);
+
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(handleCreateNewTask)}>
+      <form onSubmit={handleSubmit(handleCreateNewTaskCycle)}>
         <FormContainer>
           <label htmlFor="task">I'll work on</label>
           <TaskInput
             id="task"
             placeholder="Give your task a title"
             list="task-suggestions"
+            disabled={!!activeTask}
             {...register("task", { required: true })}
           />
           <datalist id="task-suggestions">
-            <option value="Check Akiflow" />
+            <option value="Reply to Discord messages" />
             <option value="Pick Jira tasks for the day" />
           </datalist>
           <label htmlFor="duration">for</label>
@@ -59,22 +150,29 @@ function Home() {
             type="number"
             placeholder="00"
             step={5}
-            min={5}
+            min={1}
             max={60}
+            disabled={!!activeTask}
             {...register("duration", { required: true, valueAsNumber: true })}
           />
           <span>minutes.</span>
         </FormContainer>
         <TimerContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutesCountdown[0]}</span>
+          <span>{minutesCountdown[1]}</span>
           <Colon>:</Colon>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondsCountdown[0]}</span>
+          <span>{secondsCountdown[1]}</span>
         </TimerContainer>
-        <ButtonContainer type="submit" disabled={isSubmitDisabled}>
-          <PlayIcon /> Start Task
-        </ButtonContainer>
+        {activeTask ? (
+          <StopButtonContainer type="button" onClick={handleStopTaskCycle}>
+            <StopIcon /> Cancel Task
+          </StopButtonContainer>
+        ) : (
+          <StartButtonContainer type="submit" disabled={isSubmitDisabled}>
+            <PlayIcon /> Start Task
+          </StartButtonContainer>
+        )}
       </form>
     </HomeContainer>
   );
